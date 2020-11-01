aad96d9e406d89b2cc50f860040c4f8e6051d279
leeminki <midaslmg@hufs.ac.kr>
2020-11-02 01:28:36 +0900

    Initial commit

---
+++ src/main/java/spboot/spbootpractice/SpbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package spboot.spbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spboot/spbootpractice/SpbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package spboot.spbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
